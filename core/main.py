e1e7c8b2b9307ef08d2f0d10941977e720c2fe44
Janos Tordai <tjanos88@gmail.com>
2023-06-13 21:56:32 +0200

    DEV: added crud endpoints for municipality model

---
+++ core/main.py
@@ -0,0 +1,81 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
+from core import database
+from core.models import Municipality
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/api/municipality")
+async def get_municipalities() -> list[Municipality]:
+    """
+    Retrieves all municipalities from the database and
+    returns them as a list.
+    """
+    response = await database.fetch_all_municipalities()
+    return response
+
+
+@app.get("/api/municipality/{m_id}", response_model=Municipality)
+async def get_municipality_by_id(m_id: int) -> Municipality:
+    """
+    Retrieves a municipality from the database based on the
+    given ID and returns it.
+    """
+    response = await database.fetch_municipality_by_id(m_id)
+    if response:
+        return response
+    raise HTTPException(
+        404,
+        f"There is no municipality in the database with this id: {m_id}",
+    )
+
+
+@app.post("/api/municipality", response_model=Municipality)
+async def post_municipality(municipality: Municipality) -> Municipality:
+    """
+    Creates a new municipality in the database with the provided data
+    and returns the created municipality.
+    """
+    response = await database.create_municipality(municipality)
+    if response:
+        return response
+    raise HTTPException(400, "Bad Request")
+
+
+@app.put("/api/municipality/{m_id}", response_model=Municipality)
+async def update_one_municipality(m_id: int, name: str, state: str) -> Municipality:
+    """
+    Updates the name and state fields of a municipality in the database
+    based on the given ID and returns the updated municipality as a response.
+    """
+    response = await database.update_municipality(m_id, name, state)
+    if response:
+        return response
+    raise HTTPException(
+        404,
+        f"There is no municipality in the database with this id: {m_id}",
+    )
+
+
+@app.delete("/api/municipality/{m_id}")
+async def delete_one_municipality(m_id: int):
+    """
+    Deletes a municipality from the database based on the given ID.
+    Returns a success message if the deletion is successful.
+    """
+    response = await database.remove_municipality(m_id)
+    if response:
+        return f"Successfully deleted municipality with id: {m_id}"
+    raise HTTPException(
+        404,
+        f"There is no municipality in the database with this id: {m_id}",
+    )
