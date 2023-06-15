c7f1af2da74e99cd52b02111de9538378e1c0454
Janos Tordai <tjanos88@gmail.com>
2023-06-15 23:15:39 +0200

    MAINT: added type hints

--- core/municipality/municipality_db.py
+++ core/municipality/municipality_db.py
@@ -19,7 +19,7 @@ async def fetch_all_municipalities() -> list[Municipality]:
     return municipalities
 
 
-async def fetch_municipality_by_id(m_id: int):
+async def fetch_municipality_by_id(m_id: int) -> dict[str, str] | None:
     """
     Retrieves a municipality document from the collection based on the given ID.
     """
@@ -36,7 +36,11 @@ async def create_municipality(municipality: Municipality) -> Municipality:
     return municipality
 
 
-async def update_municipality(m_id: int, name: str, state: str):
+async def update_municipality(
+    m_id: int,
+    name: str,
+    state: str,
+) -> dict[str, str] | None:
     """
     Updates the name and state fields of a municipality document
     identified by the given ID, and returns the updated document.
@@ -67,7 +71,7 @@ async def remove_municipality(m_id: int) -> bool:
     return False
 
 
-async def delete_all_municipalities():
+async def remove_all_municipalities() -> None:
     """Removes all municipalities from the database"""
     x = await municipality_collection.delete_many({})
     print(f"Removed all ({x.deleted_count}) municipalities from the database.")
@@ -76,6 +80,7 @@ async def delete_all_municipalities():
 if __name__ == "__main__":
     loop = asyncio.new_event_loop()
     try:
-        loop.run_until_complete(delete_all_municipalities())
+        loop.run_until_complete(remove_all_municipalities())
+        # loop.run_until_complete(remove_municipality(99999))
     except KeyboardInterrupt:
         pass
