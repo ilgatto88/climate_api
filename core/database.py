28ea454fe4204d106e87eab239813212bdec1dbf
Janos Tordai <tjanos88@gmail.com>
2023-06-15 13:55:39 +0200

    MAINT: renamed databases

--- core/database.py
+++ core/database.py
@@ -5,4 +5,5 @@ from core import dbconfig
 client = motor.motor_asyncio.AsyncIOMotorClient(dbconfig.DB_URI)
 
 climate_data_database = client.ClimateData
-geo_database = client.GeoDatabase
+geo = client.GeoData
+climate_data = client.ClimateData
