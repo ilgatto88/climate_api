6a711062278463ebddccd3d0b083264d297ef4f9
Janos Tordai <tjanos88@gmail.com>
2023-06-15 23:17:01 +0200

    DEV: fixed router imports, added auth router

--- core/main.py
+++ core/main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 
+from core.auth import auth_router
 from core.municipality import municipality_router
 from core.municipality_data import municipality_data_router
+
+v1_prefix = "/api/v1"
 
 app = FastAPI(
     title="climATe API",
@@ -11,8 +14,9 @@ app = FastAPI(
     version="0.1.0",
 )
 
-app.include_router(municipality_router.router)
-app.include_router(municipality_data_router.router)
+app.include_router(municipality_data_router.router, prefix=v1_prefix)
+app.include_router(municipality_router.router, prefix=v1_prefix)
+app.include_router(auth_router.router)
 
 app.add_middleware(
     CORSMiddleware,
@@ -25,4 +29,4 @@ app.add_middleware(
 @app.get("/", include_in_schema=False)
 async def index():
     """Redirects the user to the documentation page."""
-    return RedirectResponse("docs")
+    return RedirectResponse("/docs")
