b67a3d485c21cc8b178540b6e677831359d5b518
Janos Tordai <tjanos88@gmail.com>
2023-06-15 23:16:00 +0200

    DEV: added UserSchema and UserLoginSchema for auth

--- core/schemas.py
+++ core/schemas.py
@@ -1,6 +1,29 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field
+
+
+class UserSchema(BaseModel):
+    fullname: str = Field(...)
+    email: EmailStr = Field(...)
+    password: str = Field(...)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "fullname": "Joe Doe",
+                "email": "joe@example.com",
+                "password": "any",
+            }
+        }
+
+
+class UserLoginSchema(BaseModel):
+    email: EmailStr = Field(...)
+    password: str = Field(...)
+
+    class Config:
+        schema_extra = {"example": {"email": "joe@example.com", "password": "any"}}
 
 
 class Municipality(BaseModel):
