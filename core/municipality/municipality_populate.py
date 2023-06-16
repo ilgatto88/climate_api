959b30922e8bf5717727c610939a279358b9dc71
Janos Tordai <tjanos88@gmail.com>
2023-06-16 15:34:10 +0200

    DEV: added header and token

--- core/municipality/municipality_populate.py
+++ core/municipality/municipality_populate.py
@@ -1,18 +1,24 @@
 import requests
 
 from climate_data_processing import config, geodataframe_tools, static_geo
+from core.main import BASE_API_URI
 
 
-def populate(m_id: int, name: str, state: str):
-    API_URI = "http://127.0.0.1:8000/api/Municipalities/"
+def populate(m_id: int, name: str, state: str, token: str):
+    API_URI = f"{BASE_API_URI}/api/v1/Municipalities/"
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer {}".format(token),
+        "Content-Type": "application/json",
+    }
     requests.post(
-        API_URI,
-        json={"m_id": m_id, "name": name, "state": state},
+        API_URI, json={"m_id": m_id, "name": name, "state": state}, headers=headers
     )
 
 
 if __name__ == "__main__":
     # Populates database, takes municipality information from shapefile
+    token = "Testtoken"
     shape_data = geodataframe_tools.load_shapefile(config.MUNICIPALITY_SHAPEFILE)
 
     counter = 0
@@ -20,11 +26,11 @@ if __name__ == "__main__":
         m_id, name = row["g_id"], row["g_name"]
         state = static_geo.STATES[m_id[0]]
 
-        populate(m_id, name, state)
+        populate(m_id, name, state, token)
         counter += 1
 
     # add Wien
-    populate(90000, "Wien", "Wien")
+    populate(90000, "Wien", "Wien", token)
     counter += 1
 
     print(f"Added {counter} municipalities to the database.")
