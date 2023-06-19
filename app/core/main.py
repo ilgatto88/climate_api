2d1a9349964c0959b60b05c642d5c138e1441d93
Janos Tordai <tjanos88@gmail.com>
2023-06-19 16:16:03 +0200

    MAINT: app.py renamed to main.py

--- app/core/main.py
+++ app/core/main.py
@@ -50,4 +50,4 @@ async def index():
 
 
 if __name__ == "__main__":
-    uvicorn.run("app.core.app:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app.core.main:app", host="127.0.0.1", port=8000, reload=True)
