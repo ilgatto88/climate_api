16fd0f5679d15e80b2250a823c595b42a2201e90
Janos Tordai <tjanos88@gmail.com>
2023-06-20 16:28:08 +0200

    DEV: put variable collection into function

--- app/auth/auth_handler.py
+++ app/auth/auth_handler.py
@@ -4,25 +4,30 @@ import jwt
 from decouple import UndefinedValueError, config
 from jwt.exceptions import DecodeError
 
-JWT_SECRET = config("JWT_SECRET")
-JWT_ALGORITHM = config("JWT_ALGORITHM")
 
-if not isinstance(JWT_ALGORITHM, str) or not isinstance(JWT_SECRET, str):
-    raise UndefinedValueError(
-        "Please set the secret key and algorithm value in your .env file!"
-    )
+def get_jwt_variables() -> tuple[str, str]:
+    JWT_SECRET = config("JWT_SECRET")
+    JWT_ALGORITHM = config("JWT_ALGORITHM")
+
+    if not isinstance(JWT_ALGORITHM, str) or not isinstance(JWT_SECRET, str):
+        raise UndefinedValueError(
+            "Please set the secret key and algorithm value in your .env file!"
+        )
+    return (JWT_SECRET, JWT_ALGORITHM)
 
 
 def signJWT(user_id: str) -> dict[str, str]:
     payload = {"user_id": user_id, "expires": time.time() + 600}
-    token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+    jwt_secret, jwt_algorithm = get_jwt_variables()
+    token = jwt.encode(payload, jwt_secret, algorithm=jwt_algorithm)
 
     return {"access_token": token}
 
 
 def decodeJWT(token: str) -> dict | None:
+    jwt_secret, jwt_algorithm = get_jwt_variables()
     try:
-        decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+        decoded_token = jwt.decode(token, jwt_secret, algorithms=[jwt_algorithm])
         return (
             decoded_token
             if decoded_token["expires"] >= time.time()
