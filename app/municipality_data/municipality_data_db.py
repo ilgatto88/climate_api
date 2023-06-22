a56d7fe037cbf6cc63ac4dee4465ee1321ff973c
Janos Tordai <tjanos88@gmail.com>
2023-06-22 15:54:06 +0200

    test: added remove municipality data function, mainly for testing

--- app/municipality_data/municipality_data_db.py
+++ app/municipality_data/municipality_data_db.py
@@ -1,10 +1,12 @@
+from typing import Any
+
 from app.core.database import climate_data
 from app.core.models import MunicipalityData
 
 municipality_data_collection = climate_data.MunicipalityData
 
 
-async def fetch_municipality_data_by_id(m_id: int) -> MunicipalityData | None:
+async def fetch_municipality_data_by_id(m_id: int) -> dict[str, Any] | None:
     """
     Retrieves a municipality data document from the collection based on the given ID.
     """
@@ -23,3 +25,13 @@ async def create_municipality_data(
     """
     await municipality_data_collection.insert_one(municipality_data.dict())
     return municipality_data
+
+
+async def remove_municipality_data_by_id(m_id: int) -> dict[str, Any] | None:
+    """
+    Removes a municipality data document from the collection based on the given ID.
+    """
+    document = await municipality_data_collection.find_one_and_delete(
+        {"meta.municipalityId": m_id}
+    )
+    return document
