46a2a1afbde45a6431bbdcd76a3ada84636c9c23
Janos Tordai <tjanos88@gmail.com>
2023-06-24 19:45:00 +0200

    refactor: introduced MunicipalityUpdate class

--- src/municipality/models.py
+++ src/municipality/models.py
@@ -1,8 +1,10 @@
+from typing import Optional
+
 from pydantic import BaseModel, Field
 
 
 class Municipality(BaseModel):
-    """Stores municpality related data."""
+    """Stores municipality related data."""
 
     m_id: int = Field(gt=0, description="Municipality ID", lt=100000)
     name: str = Field(min_length=1, max_length=50, description="Municipality name")
@@ -12,6 +14,21 @@ class Municipality(BaseModel):
         schema_extra = {
             "example": {
                 "m_id": 99999,
+                "name": "Testcity",
+                "state": "Teststate",
+            }
+        }
+
+
+class MunicipalityUpdate(BaseModel):
+    """Stores municipality related data for update."""
+
+    name: Optional[str] = None
+    state: Optional[str] = None
+
+    class Config:
+        schema_extra = {
+            "example": {
                 "name": "Testcity",
                 "state": "Teststate",
             }
