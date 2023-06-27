761ba82f0deb217db1fa7efdd5b4384295b00861
Janos Tordai <tjanos88@gmail.com>
2023-06-27 17:52:20 +0200

    dev: added shape, removed methods since it's inefficient to load netcdf every time municipality data is calculated (if using a loop)

--- api_data_processing/models.py
+++ api_data_processing/models.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 import geopandas
 
-from api_data_processing import config, geodataframe_tools
+from api_data_processing import geodataframe_tools
 from api_data_processing.static_geo import VIENNA_GIDS
 
 
@@ -17,26 +17,11 @@ class MunicipalityDataSettings:
     analysis_end_year: int
     ensemble_start_year: int
     ensemble_end_year: int
-
-    def create_future_input_file_path(self, scenario: str) -> str:
-        """Creates the future input file path string."""
-        return (
-            f"{config.BASE_DATA_PATH}climate_data/netcdf/{scenario}/"
-            f"oeks-{scenario}-{self.climateParameter}-austria-YS.nc"
-        )
-
-    def create_historical_input_file_path(self) -> str:
-        """Creates the historical input file string."""
-        return (
-            f"{config.BASE_DATA_PATH}climate_data/netcdf/historical/"
-            f"spartacus-{self.climateParameter}-austria-YS.nc"
-        )
+    shape: geopandas.GeoDataFrame
 
     def load_geodataframe(self) -> geopandas.GeoDataFrame:
         """Loads the geodataframe for the chosen municipality."""
-        geo_df = geodataframe_tools.load_shapefile(config.MUNICIPALITY_SHAPEFILE)
-        if self.municipalityId == 90000:
-            id_list = VIENNA_GIDS
-        else:
-            id_list = [str(self.municipalityId)]
-        return geodataframe_tools.filter_geodataframe_by_ids(geo_df, id_list)
+        id_list = (
+            VIENNA_GIDS if self.municipalityId == 90000 else [str(self.municipalityId)]
+        )
+        return geodataframe_tools.filter_geodataframe_by_ids(self.shape, id_list)
