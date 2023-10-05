05bf7652913d10f52d34580d4db4915a366585e8
Janos Tordai <tjanos88@gmail.com>
2023-10-05 17:13:14 +0200

    TEST: bugfixes

--- tests/municipality_scenario_data/test_municipality_scenario_data_router.py
+++ tests/municipality_scenario_data/test_municipality_scenario_data_router.py
@@ -10,14 +10,14 @@ ENDPOINT = "/api/v1/municipalitydata/scenario"
 
 MAIN_KEYS = (
     "municipalityId",
-    "climateParameter",
     "source",
+    "climateParameter",
     "temporalResolution",
     "ensembleTimeRange",
     "modelNames",
     "rawData",
-    "statistics0D",
     "statistics1D",
+    "statistics0D",
 )
 
 
@@ -38,6 +38,7 @@ async def test_get_municipality_scenario_data_by_id_parameter_and_scenario(
     response = await client.get(f"{ENDPOINT}/rcp26/tm/10707")
     assert response.status_code == 200
     assert type(response.json()) == dict
+    print(response.json())
     assert all(key in response.json() for key in MAIN_KEYS)
 
 
